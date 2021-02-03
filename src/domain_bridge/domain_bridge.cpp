ddf9c273920a5b2e631d527751d5c62eba9f5549
Jacob Perron <jacob@openrobotics.org>
2021-02-03 15:42:41 -0800

    Support generic message types
    Integrate GenericPublisher and GenericSubscription classes.
    Now the domain bridge does not need to know about message types at compile time.
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

--- src/domain_bridge/domain_bridge.cpp
+++ src/domain_bridge/domain_bridge.cpp
@@ -21,11 +21,8 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp/executors/multi_threaded_executor.hpp>
-
 #include <rcutils/logging_macros.h>
-
-// Temporary headers for testing
-#include <std_msgs/msg/string.hpp>
+#include <rosbag2_cpp/typesupport_helpers.hpp>
 
 #include "generic_publisher.hpp"
 #include "generic_subscription.hpp"
@@ -37,6 +34,9 @@ namespace domain_bridge
 class DomainBridgeImpl
 {
 public:
+  using SubscriptionMap = std::map<std::string, std::shared_ptr<GenericSubscription>>;
+  using PublisherMap = std::map<std::string, std::shared_ptr<GenericPublisher>>;
+
   DomainBridgeImpl(size_t domain_id_a, size_t domain_id_b)
   {
     this->context_a_ = create_context_with_domain_id(domain_id_a);
@@ -53,8 +53,8 @@ public:
     this->executor_ = std::make_unique<rclcpp::executors::MultiThreadedExecutor>(executor_options);
 
     // Test bridge
-    this->bridge_from_a("chitter");
-    this->bridge_from_b("chatter");
+    this->bridge_from_a("chitter", "std_msgs/msg/String");
+    this->bridge_from_b("chatter", "std_msgs/msg/String");
   }
 
   ~DomainBridgeImpl()
@@ -83,10 +83,11 @@ public:
                   .start_parameter_event_publisher(false);
   }
 
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr create_publisher(
+  std::shared_ptr<GenericPublisher> create_publisher(
     rclcpp::Node::SharedPtr node,
-    std::map<std::string, rclcpp::Publisher<std_msgs::msg::String>::SharedPtr> & map,
-    const std::string & topic_name)
+    PublisherMap & map,
+    const std::string & topic_name,
+    const rosidl_message_type_support_t & typesupport_handle)
   {
     if (map.find(topic_name) != map.end()) {
       throw BridgeExistsError(
@@ -94,17 +95,20 @@ public:
         topic_name
       );
     }
-    // TODO: replace with generic publisher
-    map[topic_name] = node->create_publisher<std_msgs::msg::String>(
-      topic_name, 10);
+    map[topic_name] = std::make_shared<GenericPublisher>(
+      node->get_node_base_interface().get(),
+      typesupport_handle,
+      topic_name,
+      rclcpp::QoS(10));
     return map[topic_name];
   }
 
   void create_subscription(
     rclcpp::Node::SharedPtr node,
-    std::map<std::string, rclcpp::SubscriptionBase::SharedPtr> & map,
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher,
-    const std::string & topic_name)
+    SubscriptionMap & map,
+    std::shared_ptr<GenericPublisher> publisher,
+    const std::string & topic_name,
+    const rosidl_message_type_support_t & typesupport_handle)
   {
     if (map.find(topic_name) != map.end()) {
       throw BridgeExistsError(
@@ -112,28 +116,49 @@ public:
         topic_name
       );
     }
-    // TODO: replace with generic subscription
-    map[topic_name] = node->create_subscription<std_msgs::msg::String>(
+
+    // Create subscription
+    auto subscription = std::make_shared<GenericSubscription>(
+      node->get_node_base_interface().get(),
+      typesupport_handle,
       topic_name,
       rclcpp::QoS(10),
-      [publisher](std_msgs::msg::String::UniquePtr msg) {
-        RCUTILS_LOG_DEBUG_NAMED(
-          "domain_bridge", "received message '%s'", msg->data.c_str());
+      [publisher](std::shared_ptr<rclcpp::SerializedMessage> msg) {
         // Publish message into the other domain
-        publisher->publish(std::move(msg));
+        auto serialized_data_ptr = std::make_shared<rcl_serialized_message_t>(
+          msg->get_rcl_serialized_message());
+        publisher->publish(serialized_data_ptr);
       });
+    node->get_node_topics_interface()->add_subscription(subscription, nullptr);
+    map[topic_name] = subscription;
   }
 
-  void bridge_from_a(const std::string & topic_name) {
+  void bridge_from_a(const std::string & topic_name, const std::string & type_name) {
+    // Get typesupport
+    auto typesupport_library = rosbag2_cpp::get_typesupport_library(
+      type_name, "rosidl_typesupport_cpp");
+    auto typesupport_handle = rosbag2_cpp::get_typesupport_handle(
+      type_name, "rosidl_typesupport_cpp", typesupport_library);
+
     // The publisher should be created first so it is available to the subscription callback
-    auto pub = this->create_publisher(this->node_b_, this->publisher_map_b_, topic_name);
-    this->create_subscription(this->node_a_, this->subscription_map_a_, pub, topic_name);
+    auto publisher = this->create_publisher(
+      this->node_b_, this->publisher_map_b_, topic_name, *typesupport_handle);
+    this->create_subscription(
+      this->node_a_, this->subscription_map_a_, publisher, topic_name, *typesupport_handle);
   }
 
-  void bridge_from_b(const std::string & topic_name) {
+  void bridge_from_b(const std::string & topic_name, const std::string & type_name) {
+    // Get typesupport
+    auto typesupport_library = rosbag2_cpp::get_typesupport_library(
+      type_name, "rosidl_typesupport_cpp");
+    auto typesupport_handle = rosbag2_cpp::get_typesupport_handle(
+      type_name, "rosidl_typesupport_cpp", typesupport_library);
+
     // The publisher should be created first so it is available to the subscription callback
-    auto pub = this->create_publisher(this->node_a_, this->publisher_map_a_, topic_name);
-    this->create_subscription(this->node_b_, this->subscription_map_b_, pub, topic_name);
+    auto publisher = this->create_publisher(
+      this->node_a_, this->publisher_map_a_, topic_name, *typesupport_handle);
+    this->create_subscription(
+      this->node_b_, this->subscription_map_b_, publisher, topic_name, *typesupport_handle);
   }
 
   void spin() {
@@ -155,16 +180,16 @@ public:
   rclcpp::Node::SharedPtr node_b_;
 
   /// Map of topic names to publishers for domain A
-  std::map<std::string, rclcpp::Publisher<std_msgs::msg::String>::SharedPtr> publisher_map_a_;
+  PublisherMap publisher_map_a_;
 
   /// Map of topic names to publishers for domain B
-  std::map<std::string, rclcpp::Publisher<std_msgs::msg::String>::SharedPtr> publisher_map_b_;
+  PublisherMap publisher_map_b_;
 
   /// Map of topic names to subscriptions for domain A
-  std::map<std::string, rclcpp::SubscriptionBase::SharedPtr> subscription_map_a_;
+  SubscriptionMap subscription_map_a_;
 
   /// Map of topic names to subscriptions for domain B
-  std::map<std::string, rclcpp::SubscriptionBase::SharedPtr> subscription_map_b_;
+  SubscriptionMap subscription_map_b_;
 
   /// Common executor for both nodes
   std::unique_ptr<rclcpp::Executor> executor_;
