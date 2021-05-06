e5fe8ac5b500a2a3fd03a3a9613c01bd636324f8
Jacob Perron <jacob@openrobotics.org>
2021-05-05 23:37:56 -0700

    Fix lint errors
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

--- test/domain_bridge/wait_for_publisher.hpp
+++ test/domain_bridge/wait_for_publisher.hpp
@@ -12,22 +12,22 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef DOMAIN_BRIDGE__TEST__DOMAIN_BRIDGE__WAIT_FOR_PUBLISHER_HPP_
-#define DOMAIN_BRIDGE__TEST__DOMAIN_BRIDGE__WAIT_FOR_PUBLISHER_HPP_
+#ifndef DOMAIN_BRIDGE__WAIT_FOR_PUBLISHER_HPP_
+#define DOMAIN_BRIDGE__WAIT_FOR_PUBLISHER_HPP_
 
 #include <chrono>
 #include <string>
 #include <thread>
 
-#include <rclcpp/event.hpp>
-#include <rclcpp/node.hpp>
+#include "rclcpp/event.hpp"
+#include "rclcpp/node.hpp"
 
 /// Wait for a publisher to be available (or not)
 /*
  * \return true if the wait was successful, false if a timeout occured.
  */
 bool wait_for_publisher(
-  std::shared_ptr<rclcpp::Node> node,
+  rclcpp::Node::SharedPtr node,
   const std::string & topic_name,
   std::chrono::nanoseconds timeout = std::chrono::seconds(3),
   std::chrono::nanoseconds sleep_period = std::chrono::milliseconds(100))
@@ -40,7 +40,7 @@ bool wait_for_publisher(
     };
 
   while (!predicate() &&
-    time_slept <std::chrono::duration_cast<std::chrono::microseconds>(timeout))
+    time_slept < std::chrono::duration_cast<std::chrono::microseconds>(timeout))
   {
     rclcpp::Event::SharedPtr graph_event = node->get_graph_event();
     node->wait_for_graph_change(graph_event, sleep_period);
@@ -50,4 +50,4 @@ bool wait_for_publisher(
   return predicate();
 }
 
-#endif  // DOMAIN_BRIDGE__TEST__DOMAIN_BRIDGE__WAIT_FOR_PUBLISHER_HPP_
+#endif  // DOMAIN_BRIDGE__WAIT_FOR_PUBLISHER_HPP_
